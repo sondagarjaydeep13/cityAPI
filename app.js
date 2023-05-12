915a953a4e9e4a240b4aab46f04d0e1057dfb2f9
sondagarjaydeep13 <113520283+sondagarjaydeep13@users.noreply.github.com>
2023-05-12 23:37:15 +0530

    CityAPI

---
+++ app.js
@@ -0,0 +1,91 @@
+var headers = new Headers();
+headers.append(
+  "X-CSCAPI-KEY",
+  "UXF2OHQ2WjBMT1Y5Q05MQzVhNE1sT3VJSk02Y3BaNzlRNHRVMHRjZA==" // API Key
+);
+
+var requestOptions = {
+  method: "GET",
+  headers: headers,
+  redirect: "follow",
+};
+const getCountry = () => {
+  fetch(`https://api.countrystatecity.in/v1/countries`, requestOptions)
+    .then((resolve) => {
+      return resolve.json();
+    })
+    .then((result) => {
+      var row = "";
+      for (var i = 0; i < result.length; i++) {
+        row =
+          row +
+          "<option value=" +
+          result[i].iso2 +
+          ">" +
+          result[i].name +
+          "<option/>";
+      }
+      country.innerHTML = row;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+var countrycode;
+const getState = (ccode) => {
+  countrycode = ccode;
+  fetch(
+    `https://api.countrystatecity.in/v1/countries/${ccode}/states`,
+    requestOptions
+  )
+    .then((resolve) => {
+      return resolve.json();
+    })
+    .then((result) => {
+      var row = "";
+      for (var i = 0; i < result.length; i++) {
+        row =
+          row +
+          "<option value=" +
+          result[i].iso2 +
+          ">" +
+          result[i].name +
+          "<option/>";
+      }
+      state.innerHTML = row;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+var citycodes;
+const getCity = (scode) => {
+  citycodes = scode;
+
+  fetch(
+    `https://api.countrystatecity.in/v1/countries/${countrycode}/states/${scode}/cities`,
+    requestOptions
+  )
+    .then((resolve) => {
+      return resolve.json();
+    })
+    .then((result) => {
+      var row = "";
+
+      for (var i = 0; i < result.length; i++) {
+        row =
+          row +
+          "<option value=" +
+          result[i].name +
+          ">" +
+          result[i].name +
+          "</option>";
+        // console.log(result[i]);
+      }
+      city1.innerHTML = row;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
